8f9a51c31be87f1e4cbbc7ce14d2a641b60ffee3
j-g <baconian@posteo.de>
2019-12-20 15:09:45 +0100

    Added check of calculted simmilarity and stored scores and annotations.

--- main.py
+++ main.py
@@ -5,13 +5,14 @@ from sklearn.metrics import mean_squared_error
 from sklearn import preprocessing
 import argparse
 import algs
+import hashlib
 import util
 from collections import OrderedDict
 from sklearn.metrics.pairwise import cosine_similarity
 import time
 import json
-from json import JSONEncoder
-
+from os import path
+import pickle
 
 class Dataset:
     def __init__(self, name):
@@ -19,6 +20,7 @@ class Dataset:
         self.data = []
         self.ids = []
         self.annots = None
+        self.phrase_vecs = {}
 
     def __str__(self):
         output = ""
@@ -40,10 +42,23 @@ class Dataset:
             for line in f.readlines():
                 self.data.append(line)
                 self.ids.append(i)
-                i+=1
+                i += 1
+        data_str = str(self.data)
+        data_str = data_str.encode("utf-8")
+        self.hash = hashlib.md5(data_str).hexdigest()
+
+    def calc_vecs(self, alg):
+        """Precalculates the vectors and stores them in memory."""
+        if not alg.trained:
+            alg.train(self.data)
+        self.phrase_vecs[alg] = []
+        print("Creating Vectors")
+        for item in self.data:
+            self.phrase_vecs[alg].append(alg.create_vec(item))
+
 
     def run_annot(self):
-        """Randomly selects 3 senteces from self.data and asks the user to evaluate which sentence is more simmilar to a given reference sentence.
+        """Randomly selects 3 sentences from self.data and asks the user to evaluate which sentence is more simmilar to a given reference sentence.
         The results are stored in a 3d array:
             1st dim: reference sentence id
             2nd dim: id with the more simmilar sentence
@@ -51,22 +66,55 @@ class Dataset:
         If you wanted to compare two sentences you could compare:
         self.annot[x][y][z] to self.annot[x][z][y]"""
         run = True
-        if self.annots == None:
-            id_len = len(self.ids)
+        alg = algs.spacy_sem_sim(language="german")
+        id_len = len(self.ids)
+        if path.exists("./data/{}-scores.json".format(self.hash)):
+            with open("./data/{}-scores.json".format(self.hash)) as f:
+                self.refscores = np.array(json.load(f))
+        else:
+            self.calc_vecs(alg)
+            self.refscores = np.zeros((id_len+1, id_len+1))
+            for col in range(len(self.data)):
+                for row in range(len(self.data)):
+                    self.refscores[row][col] = alg.compare(
+                        self.phrase_vecs[alg][row], self.phrase_vecs[alg][col])
+            with open("./data/{}-scores.json".format(self.hash),"w+") as f:
+               json.dump(self.refscores.tolist(),f, indent =2)
+        if path.exists("./data/{}-annots.json".format(self.hash)):
+            with open("./data/{}-annots.json".format(self.hash)) as f:
+                self.annots = np.array(json.load(f))
+        else:
             self.annots = np.zeros((id_len,id_len,id_len))
         while run:
             sentence_ids = np.random.choice(self.ids, 3)
             sentences = np.array(self.data)[sentence_ids]
-            print("Reference:\n {}\n\n".format(sentences[0]),"Sentence 1:\n {}\n\n".format(sentences[1]),"Sentence 2:\n {}".format(sentences[2]))
-            answer = input().lower()
-            if answer == "quit" or answer == "q":
-                run = False
-            elif answer == "1":
-                self.annots[sentence_ids[0],sentence_ids[1],sentence_ids[2]] += 1
-                print("Sentence 1 is more similar to reference sentence.\n" )
-            elif answer == "2":
-                self.annots[sentence_ids[0],sentence_ids[2],sentence_ids[1]] += 1
-                print("Sentence 2 is more similar to reference sentence.\n" )
+            answer = ""
+            while answer not in ["quit", "q", "s", "1", "2"]:
+                print("Reference:\n {}\n\n".format(sentences[0]), "Sentence 1:\n {}\n\n".format(
+                    sentences[1]), "Sentence 2:\n {}".format(sentences[2]))
+                answer = input().lower()
+                if answer == "quit" or answer == "q":
+                    run = False
+                elif answer == "1":
+                    print("Sentence 1 is more similar to reference sentence.\n")
+                    self.annots[sentence_ids[0],
+                                sentence_ids[1], sentence_ids[2]] += 1
+                    if self.refscores[sentence_ids[0]][sentence_ids[1]] > self.refscores[sentence_ids[0]][sentence_ids[2]]:
+                        print("True!")
+                    else:
+                        print("False!")
+                elif answer == "2":
+                    self.annots[sentence_ids[0],
+                                sentence_ids[2], sentence_ids[1]] += 1
+                    print("Sentence 2 is more similar to reference sentence.\n")
+                    if self.refscores[sentence_ids[0]][sentence_ids[2]] > self.refscores[sentence_ids[0]][sentence_ids[1]]:
+                        print("True!")
+                    else:
+                        print("False!")
+                else:
+                    print("Unrecognized answer.")
+        with open("./data/{}-annots.json".format(self.hash),"w+") as f:
+               json.dump(self.annots.tolist(),f, indent =2)
         
 
 
@@ -240,4 +288,4 @@ if __name__ == "__main__":
         benchmark(create_alg_list(args.algs))
     db = Dataset("nachrichten") 
     db.load_data("./data/nachrichten.txt")
-    db.run_eval()
+    db.run_annot()
