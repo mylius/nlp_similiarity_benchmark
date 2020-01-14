a3892120a5c3d3de6548d4c94e74ce84dd455e6e
Jan-Gabriel Mylius <baconian@posteo.de>
2020-01-14 03:31:51 +0100

    Added some evaluation metrics.

--- annotate.py
+++ annotate.py
@@ -5,6 +5,7 @@ import json
 from os import path
 from collections import defaultdict
 from scipy import special
+from itertools import permutations
 
 
 class Dataset:
@@ -33,6 +34,9 @@ class Dataset:
         self.data = []
         self.annots = defaultdict(int)
         self.phrase_vecs = {}
+        self.correctness = []
+        self.rec_thresh = 0.12
+        self.strong_disagreement = []
 
     def __str__(self):
         """Returns all strings in the dataset."""
@@ -109,14 +113,7 @@ class Dataset:
         run = True
         alg = algs.spacy_sem_sim(language="german")
         self.id_len = len(self.data)
-        if path.exists("./data/{}-scores.json".format(self.hash)):
-            with open("./data/{}-scores.json".format(self.hash)) as f:
-                self.refscores = np.array(json.load(f))
-        else:
-            self.calc_scores(alg)
-        if path.exists("./data/{}-annots.json".format(self.hash)):
-            with open("./data/{}-annots.json".format(self.hash)) as f:
-                self.annots = defaultdict(int, json.load(f))
+        self.load_files()
         while run:
             # check if all combinations have been annotated:
             if len(self.annots) >= self.id_len*special.binom(self.id_len-1, 2):
@@ -143,25 +140,77 @@ class Dataset:
                     print("Sentence 1 is more similar to reference sentence.\n")
                     self.annots[str((sentence_ids[0],
                                      sentence_ids[1], sentence_ids[2]))] += 1
-                    if self.refscores[sentence_ids[0]][sentence_ids[1]] > self.refscores[sentence_ids[0]][sentence_ids[2]]:
-                        print("Matching expectation!")
-                    else:
-                        print("Unexpected annotation!")
+                    self.save_correct(
+                        sentence_ids[0], sentence_ids[1], sentence_ids[2])
                 elif answer == "2":
                     self.annots[str((sentence_ids[0],
                                      sentence_ids[2], sentence_ids[1]))] += 1
                     print("Sentence 2 is more similar to reference sentence.\n")
-                    if self.refscores[sentence_ids[0]][sentence_ids[2]] > self.refscores[sentence_ids[0]][sentence_ids[1]]:
-                        print("Matching expectation!")
-                    else:
-                        print("Unexpected annotation!")
+                    self.save_correct(
+                        sentence_ids[0], sentence_ids[2], sentence_ids[1])
                 else:
                     print("Unrecognized answer.")
+        self.save_results()
+
+    def save_results(self):
+        """ Saves the current results of annotations, their correctness and strong disagreements. """
         with open("./data/{}-annots.json".format(self.hash), "w+") as f:
             json.dump(self.annots, f, indent=2)
+        with open("./data/{}-correctness.json".format(self.hash), "w+") as f:
+            json.dump(self.correctness, f, indent=2)
+        with open("./data/{}-disagreement.json".format(self.hash), "w+") as f:
+            json.dump(self.strong_disagreement, f, indent=2)
+
+    def load_files(self):
+        """ Saves the saved results of annotations, their correctness and strong disagreements. """
+        if path.exists("./data/{}-scores.json".format(self.hash)):
+            with open("./data/{}-scores.json".format(self.hash)) as f:
+                self.refscores = np.array(json.load(f))
+        else:
+            self.calc_scores(alg)
+        if path.exists("./data/{}-annots.json".format(self.hash)):
+            with open("./data/{}-annots.json".format(self.hash)) as f:
+                self.annots = defaultdict(int, json.load(f))
+        if path.exists("./data/{}-correctness.json".format(self.hash)):
+            with open("./data/{}-correctness.json".format(self.hash)) as f:
+                self.correctness = json.load(f)
+        if path.exists("./data/{}-disagreement.json".format(self.hash)):
+            with open("./data/{}-disagreement.json".format(self.hash)) as f:
+                self.strong_disagreement = json.load(f)
+                print(self.strong_disagreement)
+
+    def save_correct(self, ref_id, sent_id1, sent_id2):
+        """
+        Checks if the users annotations matches the algorithms and saves whether the annotation matched.
+        If the algorithm is very confident and the user disagrees the triplet is stored.
+
+        Parameters
+        ----------
+        ref_id : Id of the reference sentence.
+        sent_id1 : Id of the sentence the user thought to be more like the reference sentence.
+        sent_id2 : Id of the sentence the user thought to be less like the reference sentence.
+        """
+        if self.refscores[ref_id][sent_id1] > self.refscores[ref_id][sent_id2]:
+            print("Matching expectation!")
+            self.correctness.append(1)
+        else:
+            print("Unexpected annotation!")
+            self.correctness.append(0)
+
+        if self.refscores[ref_id][sent_id2] - self.refscores[ref_id][sent_id1] > self.rec_thresh:
+            print("Registered strong disagreement with algorithm")
+            self.strong_disagreement.append(str((ref_id,sent_id1,sent_id2)))
+
+
+    def evaluate(self):
+        """Prints the percentage of correct annotations and a list of pairings where the user disagreed eventhough the algorithm had a clear preference."""
+        if len(self.correctness) > 0:
+            print("Correct percentage is: {}%".format(round(sum(self.correctness)/len(self.correctness)*100,2)))
+            print(self.strong_disagreement)
 
 
 if __name__ == "__main__":
     db = Dataset("nachrichten")
     db.load_data("./data/test.txt")
     db.run_annot()
+    db.evaluate()
